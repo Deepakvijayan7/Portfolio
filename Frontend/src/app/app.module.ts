897016489501d4f6c8fd52aba3c4c81b253adfec
Deepakvijayan7 <102506209+Deepakvijayan7@users.noreply.github.com>
2022-07-09 17:18:00 +0530

    Add files via upload

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { WorksComponent } from './works/works.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannerComponent,
+    ServicesComponent,
+    FooterComponent,
+    HomeComponent,
+    HeaderComponent,
+    TestimonialComponent,
+    AboutComponent,
+    ContactComponent,
+    WorksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Frontend/src/app/testimonial.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient,HttpResponse } from '@angular/common/http';
+import { response } from 'express';
+@Injectable({
+  providedIn: 'root'
+})
+export class TestimonialService {
+private _getUrl="/testimonial"
+  constructor(private http:HttpClient) { }
+  getTestimonial(){
+    return this.http.get("/testimonial");
+  }
+  // getTestimonial(){
+  //   return this.http.get("http://localhost:8080/testimonial");
+  // }
+}
